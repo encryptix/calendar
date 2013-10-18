2338d41edbdaa5e2238bddcb028d618fa3f13d71
Raymond Noonan <r4y@live.ie>
2013-10-18 11:23:08 +0100

    Modified notes code for calendar

--- backend/calendar_log.py
+++ backend/calendar_log.py
@@ -1,45 +1,53 @@
 from db import DB
 
-class Notes:
+class CalendarLog:
     #Database names
-    database = "NOTES"
-    note_id = "id"
+    database = "CALENDAR"
+    entry_id = "id"
     data = "data"
-    name= "name"
+    date= "date"
     password = "password"
-    #Actual Note ID
-    noteID = None
+    #Actual log ID
+    logID = None
 
-    def __init__(self,noteID):
-        self.DB = DB(self.database, self.note_id)
-        self.noteID = noteID
+    def __init__(self,logID):
+        self.DB = DB(self.database, self.entry_id)
+        self.logID = logID
     
-    def noteID(self):
-        return noteID
+    def logID(self):
+        return logID
 
     def index(self):
-        return self.DB.select_all([self.note_id ,self.name],None,None,None)
-
-    def select_note(self):
-        row = self.DB.select_one(self.noteID, [self.data])
-        if row:
-            return str(row[0])
-        return None
+        return self.DB.select_all([self.entry_id ,self.date],None,None,None)
     
-    def update_note(self, text):
-        return self.DB.update(self.noteID,[self.data],[text])
+    def select_log(self):
+        row = self.DB.select_one(self.logID, [self.data, self.date])
+        return row
+
+    def select_logs_range(self,start,end,limit):
+        if start and end:
+            return self.DB.select_all([self.entry_id ,self.date, self.data],[self.date+" >=", "AND "+self.date+" <="],[start,end],limit)
+        elif start:
+            return self.DB.select_all([self.entry_id ,self.date, self.data],[self.date+" >="],[start],limit)
+        elif end:
+            return self.DB.select_all([self.entry_id ,self.date, self.data],[self.date+" <="],[end],limit)
+        else:
+            return self.DB.select_all([self.entry_id ,self.date, self.data],None,None,limit)
+
+    def update_log(self, text):
+        return self.DB.update(self.logID,[self.data],[text])
     
-    def update_note_name(self,name):
-        return self.DB.update(self.noteID,[self.name],[name])
+    def update_log_date(self,date):
+        return self.DB.update(self.logID,[self.date],[date])
 
-    def add_note(self,text,name):
-        return self.DB.insert([self.data,self.name,self.password],[text,name,None])
+    def add_log(self,text,date):
+        return self.DB.insert([self.data,self.date,self.password],[text,date,None])
 
-    def remove_note(self):
-        return self.DB.remove(self.noteID)
+    def remove_log(self):
+        return self.DB.remove(self.logID)
 
     def check_password(self, password):
-        row = self.DB.select_one(self.noteID,[self.password])
+        row = self.DB.select_one(self.logID,[self.password])
         actual_password = row[0]
         if actual_password != None:
             actual_password = str(actual_password)
@@ -49,10 +57,10 @@ class Notes:
         return False
 
     def set_password(self,new_pass):
-        return self.DB.update(self.noteID,[self.password],[new_pass])
+        return self.DB.update(self.logID,[self.password],[new_pass])
 
     def is_locked(self):
-        row = self.DB.select_one(self.noteID,[self.password])
+        row = self.DB.select_one(self.logID,[self.password])
         password = row[0]
         if password==None or len(str(password)) == 0:
             return False
--- backend/interface.py
+++ backend/interface.py
@@ -1,63 +1,77 @@
 from helpers import FormHelpers
 from helpers import NumberHelpers
 from helpers import GeneralHelpers
-from notes_backend import Notes
+from calendar_log import CalendarLog
 
 def do_index():
-    n = Notes("")
+    cal = CalendarLog("")
     #will returns a list of row_id's/None
-    rows =  n.index()
-    return_str = "{\"notes\": ["
+    rows =  cal.index()
+    return_str = "{\"entries\": ["
     for row in rows:
-        return_str += "{"+GeneralHelpers.createJSONEntry("noteID",str(row[0]))+","
-        return_str += GeneralHelpers.createJSONEntry("noteName",str(row[1]))+"},"
+        return_str += "{"+GeneralHelpers.createJSONEntry("logID",str(row[0]))+","
+        return_str += GeneralHelpers.createJSONEntry("logDate",str(row[1]))+"},"
 
     #if there was content parse off the last ,
     if rows:
          return_str = return_str[:-1]
     return return_str+"]}"
 
-def do_select(n):
-    #returns note/None
-    note = n.select_note()
-    if note:
-        return "{"+GeneralHelpers.createJSONEntry("noteID",n.noteID)+", "+GeneralHelpers.createJSONEntry("note",note)+"}"
-    return "<no_note>"
+def do_select(cal):
+    #returns note/Log
+    log = cal.select_log()
+    if log:
+        return "{"+GeneralHelpers.createJSONEntry("logID",cal.logID)+", "+GeneralHelpers.createJSONEntry("logDate",log[1])+", "+GeneralHelpers.createJSONEntry("log",log[0])+"}"
+    return "<no_log>"
 
-def do_update(n, text):
+def do_select_range(start,end,limit):
+    cal = CalendarLog("")
+    logs = cal.select_logs_range(start,end,limit)
+    return_str = "{\"entries\": ["
+    for row in logs or []:
+        return_str += "{"+GeneralHelpers.createJSONEntry("logID",str(row[0]))+","
+        return_str += GeneralHelpers.createJSONEntry("logDate",str(row[1]))+","
+        return_str += GeneralHelpers.createJSONEntry("log",str(row[2]))+"},"
+
+    #if there was content parse off the last ,
+    if logs:
+         return_str = return_str[:-1]
+    return return_str+"]}"
+
+def do_update(cal, text):
     #returns boolean
-    if n.update_note(text):
+    if cal.update_log(text):
         return "<success>"
     else:
         return "<error>"
 
-def do_update_name(n,name):
+def do_update_date(cal,date):
     #returns boolean
-    if n.update_note_name(name):
+    if cal.update_log_date(date):
         return "<success>"
     else:
         return "<error>"
 
-def do_delete(n):
+def do_delete(cal):
     #returns boolean
-    if n.remove_note():
+    if cal.remove_log():
         #return the note id
-        return "{"+GeneralHelpers.createJSONEntry("noteID",n.noteID)+"}"
+        return "{"+GeneralHelpers.createJSONEntry("logID",cal.logID)+"}"
     else:
         return "<error>"
 
-def do_add_note(data,name):
-    n = Notes(None)
-    note_id = n.add_note(data,name)
-    return "{"+GeneralHelpers.createJSONEntry("noteID",note_id)+", "+GeneralHelpers.createJSONEntry("noteName",name)+"}"
+def do_add_log(data,date):
+    cal = CalendarLog(None)
+    log_id = cal.add_log(data,date)
+    #Why return date?
+    return "{"+GeneralHelpers.createJSONEntry("logID",log_id)+", "+GeneralHelpers.createJSONEntry("logDate",date)+"}"
 
 def do_lock(n,new_pass):
     setpw =  n.set_password(new_pass)
     locked = "false"
     if setpw:
         locked = "true"
-    return "{"+GeneralHelpers.createJSONEntry("noteID",n.noteID)+", "+GeneralHelpers.createJSONEntry("isLocked",locked)+"}"
-
+    return "{"+GeneralHelpers.createJSONEntry("logID",n.logID)+", "+GeneralHelpers.createJSONEntry("isLocked",locked)+"}"
 
 def do_unlock(n, current_pass):
     if n.check_password(current_pass):
@@ -65,13 +79,13 @@ def do_unlock(n, current_pass):
         locked = "true"
         if setpw:
             locked = "false"
-        return "{"+GeneralHelpers.createJSONEntry("noteID",n.noteID)+", "+GeneralHelpers.createJSONEntry("isLocked",locked)+"}"
+        return "{"+GeneralHelpers.createJSONEntry("logID",n.logID)+", "+GeneralHelpers.createJSONEntry("isLocked",locked)+"}"
     else:
-        return "{"+GeneralHelpers.createJSONEntry("noteID",n.noteID)+", "+GeneralHelpers.createJSONEntry("isLocked","true")+"}"
+        return "{"+GeneralHelpers.createJSONEntry("logID",n.logID)+", "+GeneralHelpers.createJSONEntry("isLocked","true")+"}"
 
 def application(environ, start_response):
     is_get = False
-    GET_allowed=[]
+    GET_allowed=["view_range"]
     try:
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
         # When the method is POST the query string will be sent
@@ -102,42 +116,64 @@ def application(environ, start_response):
     
     elif action == "add":
         data = FormHelpers.get_input(d,'data')
-        name = FormHelpers.get_input(d,'name')
-        if data and len(data) > 0 and name and len(name) >0:
-            response_body = do_add_note(data,name)
-    #The rest of these require the notes to be unlocked
-    note_id = FormHelpers.get_input(d,'id')
-    if NumberHelpers.is_positive_integer(note_id):
-        note = Notes(note_id)
-        if note.is_locked():
-            print "Note "+str(note_id)+"is locked"
+        date = FormHelpers.get_input(d,'date')
+        if data and len(data) > 0 and date and len(date) >0:
+            response_body = do_add_log(data,date)
+    elif action =="view_range":
+        print "view range"
+        limit = FormHelpers.get_input(d,'limit')
+        limit_param = None
+        if limit and NumberHelpers.is_positive_integer(limit):
+            limit_param = limit
+
+        start_date = FormHelpers.get_input(d,'start')
+        start_param = None
+        print "start date"+str(start_date)
+        if start_date and NumberHelpers.is_positive_integer(start_date):
+            start_param = start_date
+
+        end_date = FormHelpers.get_input(d,'end')
+        end_param = None
+        print "end date"+str(end_date)
+        if end_date and NumberHelpers.is_positive_integer(end_date):
+            end_param = end_date            
+
+        print "response"
+        response_body = do_select_range(start_param,end_param,limit_param)
+        
+    #The rest of these require the logs to be unlocked
+    log_id = FormHelpers.get_input(d,'id')
+    if NumberHelpers.is_positive_integer(log_id):
+        log = CalendarLog(log_id)
+        if log.is_locked():
+            print "log "+str(log_id)+"is locked"
             if action == "unlock":
                 password = str(FormHelpers.get_input(d,'password'))
                 if password and len(password) > 0:
-                    response_body = do_unlock(note,password)
+                    response_body = do_unlock(log,password)
             else:
-                response_body = "{"+GeneralHelpers.createJSONEntry("noteID",note_id)+", "+GeneralHelpers.createJSONEntry("isLocked","true")+"}"
+                response_body = "{"+GeneralHelpers.createJSONEntry("logID",log_id)+", "+GeneralHelpers.createJSONEntry("isLocked","true")+"}"
 
         elif action =="view":
-            response_body = do_select(note)
+            response_body = do_select(log)
 
         elif action == "delete":
-            response_body = do_delete(note)
+            response_body = do_delete(log)
             
         elif action == "update":
             data = FormHelpers.get_input(d,'data')
             if data:
-                response_body = do_update(note,data)       
+                response_body = do_update(log,data)       
         
-        elif action=="update_name":
-            name = FormHelpers.get_input(d,'name')
-            if name and len(name) > 0:
-                response_body = do_update_name(note,name)
+        elif action=="update_date":
+            date = FormHelpers.get_input(d,'date')
+            if date and len(date) > 0:
+                response_body = do_update_date(log,date)
 
         elif action=="lock":
             password = FormHelpers.get_input(d,'password')
             if password and len(password) > 0:
-                response_body = do_lock(note,password)
+                response_body = do_lock(log,password)
 
     status = '200 OK'
     response_headers = [('Content-Type', 'text/html'),('Content-Length', str(len(response_body)))]
